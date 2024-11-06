7b6fe26ee4d48bcecb9293827a432d28f0962464
o.denezhkina <o.denezhkina@tinkoff.ru>
2024-11-06 15:55:02 +0300

    smth

--- src/main/kotlin/kpfu/itis/odenezhkina/databasequeriesoptimizer/features/optimization/SqlQueryOptimizer.kt
+++ src/main/kotlin/kpfu/itis/odenezhkina/databasequeriesoptimizer/features/optimization/SqlQueryOptimizer.kt
@@ -6,10 +6,17 @@ import kpfu.itis.odenezhkina.databasequeriesoptimizer.features.schema.CalciteSch
 import kpfu.itis.odenezhkina.databasequeriesoptimizer.features.schema.RoomSchemaParser
 import kpfu.itis.odenezhkina.databasequeriesoptimizer.features.settings.PluginSettings
 import kpfu.itis.odenezhkina.databasequeriesoptimizer.features.settings.data
+import org.apache.calcite.adapter.enumerable.EnumerableConvention
+import org.apache.calcite.adapter.enumerable.EnumerableRules
+import org.apache.calcite.adapter.enumerable.EnumerableTableScanRule
+import org.apache.calcite.config.Lex
 import org.apache.calcite.plan.ConventionTraitDef
+import org.apache.calcite.plan.volcano.AbstractConverter
+import org.apache.calcite.rel.RelCollationTraitDef
 import org.apache.calcite.rel.RelNode
 import org.apache.calcite.rel.rel2sql.RelToSqlConverter
 import org.apache.calcite.rel.rules.CoreRules
+import org.apache.calcite.schema.SchemaPlus
 import org.apache.calcite.sql.SqlDialect
 import org.apache.calcite.sql.SqlNode
 import org.apache.calcite.sql.SqlWriterConfig
@@ -28,54 +35,61 @@ class SqlQueryOptimizerImpl(
     private val calciteSchemaMapper: CalciteSchemaMapper
 ) : SqlQueryOptimizer {
 
+    private val cachedSchemas: MutableMap<String, SchemaPlus> = mutableMapOf()
+
     override fun optimize(sql: String): OptimizationResult {
         return try {
-            val settingsState = PluginSettings.getInstance().state
-            val schemeDirectory =
-                settingsState.databaseSchemesDirectory.data() ?: return OptimizationResult.Error(
-                    IllegalStateException("No database scheme directory: set it in plugin settings")
-                )
-            val schemeVersion =
-                settingsState.databaseVersion.data() ?: return OptimizationResult.Error(
-                    IllegalStateException("No database scheme version: set it in plugin settings")
-                )
-            val fullSchemePath = "${schemeDirectory}/${schemeVersion}.json"
-            val roomSchema = roomSchemaParser.parse(fullSchemePath).getOrNull()
-                ?: return OptimizationResult.Error(IllegalStateException("Cannot parse room scheme, check settings"))
-
-            val scheme = calciteSchemaMapper
-                .mapToCalciteSchema(roomSchema)
-                .getOrNull()
-                ?: return OptimizationResult.Error(IllegalStateException("Invalid room scheme, cannot map it to calcite"))
-
-            val sqlParserConfig = SqlParser.config().withCaseSensitive(false)
-            val config: FrameworkConfig = Frameworks
-                .newConfigBuilder()
-                .defaultSchema(scheme)
-                .parserConfig(sqlParserConfig)
-                .traitDefs(listOf(ConventionTraitDef.INSTANCE))
-                .ruleSets(
-                    RuleSets.ofList(
-                        CoreRules.FILTER_INTO_JOIN,
-                        CoreRules.JOIN_COMMUTE,
-                        CoreRules.PROJECT_MERGE
-                    )
-                )
-                .build()
-            val planner: Planner = Frameworks.getPlanner(config)
+            val planner = createPlanner()
 
             val parsedTree: SqlNode = planner.parse(sql)
-                ?: return OptimizationResult.Error(IllegalStateException("Cannot parse tree for $sql"))
+                ?: error("Cannot parse tree for $sql")
             val validatedSql: SqlNode = planner.validate(parsedTree)
-                ?: return OptimizationResult.Error(IllegalStateException("Cannot validate parsed tree for $sql"))
+                ?: error("Cannot validate parsed tree for $sql")
             val relAlgRepresentation = planner.rel(validatedSql).rel
-                ?: return OptimizationResult.Error(IllegalStateException("Cannot build relation algebra representation for $sql"))
+                ?: error("Cannot build relation algebra representation for $sql")
 
-            OptimizationResult.Success(convertRelAlgebraRepresentationToSQL(relAlgRepresentation))
+            val relOptPlanner = relAlgRepresentation.cluster.planner.apply {
+                val traitSet = relAlgRepresentation.traitSet.plus(EnumerableConvention.INSTANCE)
+                val convertedRelAlgRepresentation = this.changeTraits(relAlgRepresentation, traitSet)
+                root = convertedRelAlgRepresentation
+            }
+            val optimizedRelNode = relOptPlanner.findBestExp()
+
+            OptimizationResult.Success(convertRelAlgebraRepresentationToSQL(optimizedRelNode))
         } catch (e: Exception) {
             logger.error(e)
             OptimizationResult.Error(e)
         }
+    }
+
+    private fun createPlanner(): Planner {
+        val schema = loadSchema()
+        val sqlParserConfig = SqlParser
+            .config()
+            .withLex(Lex.MYSQL)
+            .withCaseSensitive(false)
+        val config: FrameworkConfig = Frameworks.newConfigBuilder()
+            .defaultSchema(schema)
+            .parserConfig(sqlParserConfig)
+            .traitDefs(ConventionTraitDef.INSTANCE, RelCollationTraitDef.INSTANCE)
+            .ruleSets(
+                RuleSets.ofList(
+                    CoreRules.FILTER_INTO_JOIN,
+                    CoreRules.JOIN_COMMUTE,
+                    CoreRules.PROJECT_MERGE,
+                    EnumerableRules.ENUMERABLE_PROJECT_RULE,
+                    EnumerableRules.ENUMERABLE_FILTER_RULE,
+                    EnumerableRules.ENUMERABLE_SORT_RULE,
+                    EnumerableRules.ENUMERABLE_LIMIT_RULE,
+                    EnumerableRules.ENUMERABLE_JOIN_RULE,
+                    EnumerableRules.ENUMERABLE_TABLE_SCAN_RULE,
+                    AbstractConverter.ExpandConversionRule.INSTANCE,
+                    EnumerableTableScanRule.
+                )
+            )
+            .costFactory(null)
+            .build()
+        return Frameworks.getPlanner(config) ?: error("Cannot create planner")
     }
 
 
@@ -95,6 +109,30 @@ class SqlQueryOptimizerImpl(
         return SqlPrettyWriter(sqlWriterConfig).apply {
             sqlNode.unparse(this, 0, 0)
         }.toString()
+    }
+
+    private fun loadSchema(): SchemaPlus {
+        val settingsState = PluginSettings.getInstance().state
+        val schemeDirectory = settingsState.databaseSchemesDirectory.data()
+            ?: error("No database scheme directory: set it in plugin settings")
+        val schemeVersion = settingsState.databaseVersion.data()
+            ?: error("No database scheme version: set it in plugin settings")
+        val fullSchemePath = "${schemeDirectory}/${schemeVersion}.json"
+
+        val schema = cachedSchemas.getOrElse(fullSchemePath){
+            val roomSchema = roomSchemaParser
+                .parse(fullSchemePath)
+                .getOrNull()
+                ?: error("Cannot parse room scheme, check settings")
+
+            val schemaPlus = calciteSchemaMapper
+                .mapToCalciteSchema(roomSchema)
+                .getOrNull()
+                ?: error("Invalid room scheme, cannot map it to calcite")
+            cachedSchemas[fullSchemePath] = schemaPlus
+            schemaPlus
+        }
+        return schema
     }
 
 }
